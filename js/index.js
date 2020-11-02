e8e4a505550457f65aec2b89f1e594b865d35af1
Antoine <antoine.lorin2@gmail.com>
2020-11-02 21:09:55 +0100

    Modif

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import * as THREE from "./three.js-master/build/three.module.js";
+import { OrbitControls } from './three.js-master/examples/jsm/controls/OrbitControls.js';
 
 var camera, scene, renderer;
 var boite;
@@ -11,32 +12,51 @@ camera = new THREE.PerspectiveCamera(
   100,
   window.innerWidth / window.innerHeight,
   1,
-  1000
+  10000
 );
-camera.position.z = 600;
-camera.position.y = 100;
+camera.position.z = 2000;
+camera.position.y = 200;
 
-var texture = new THREE.TextureLoader().load("./../ThreeJS/téléchargement.jpg");
+var texture = new THREE.TextureLoader().load("./../texture/téléchargement.jpg");
 
-var geometry = new THREE.BoxBufferGeometry(200, 200, 200);
-var material = new THREE.MeshBasicMaterial({ map: texture });
+let Murs = []
 
-boite = new THREE.Mesh(geometry, material);
 
-geometry = new THREE.CylinderGeometry(150, 150, 300, 100);
-cylindre = new THREE.Mesh(geometry, material);
+var geometry = new THREE.BoxBufferGeometry(100, 300, 2000);
+var material = new THREE.MeshBasicMaterial({ map: texture })
+let mur = new THREE.Mesh(geometry, material)
 
-geometry = new THREE.ConeGeometry(200, 150, 60);
-cone = new THREE.Mesh(geometry, material);
+Murs.push(mur)
+
+geometry = new THREE.BoxBufferGeometry(100, 300, 1000);
+mur = new THREE.Mesh(geometry, material)
+Murs.push(mur)
 
 let tour = new THREE.Group()
-//scene.add(boite);
-tour.add(cylindre);
-tour.add(cone);
+
+Murs.forEach((elem) => {
+  tour.add(elem);
+})
+
+
+let toure = createTower(150,500,150)
+let toure2 = createTower(120,400,150)
+
+
+
+tour.add(toure);
+tour.add(toure2)
 
 scene.add(tour)
 
-cone.position.y = 300 - 75;
+Murs[0].position.set(0, 150, 2000 / 2 + 170 / 2)
+
+
+Murs[1].position.set(500, 150, 2000 + 120/2)
+Murs[1].rotateY(THREE.Math.degToRad(90))
+
+
+toure2.position.z = 2000 + 120/2;
 
 renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -46,11 +66,42 @@ document.body.appendChild(renderer.domElement);
 renderer.setClearColor(0xeeeeee);
 animate();
 
+
+
+
+
+
 function animate() {
   requestAnimationFrame(animate);
 
-  // tour.rotation.x += 0.005;
-  // tour.rotation.y += 0.01;
+  //  tour.rotation.x += 0.005;
+  //  tour.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 }
+
+
+function createTower(largeur, hauteurCylindre, hauteurCone) {
+  var texture = new THREE.TextureLoader().load("./../texture/téléchargement.jpg");
+  var material = new THREE.MeshBasicMaterial({ map: texture });
+
+  let geometry = new THREE.CylinderGeometry(largeur, largeur, hauteurCylindre, 100);
+  let cylindre = new THREE.Mesh(geometry, material);
+
+  geometry = new THREE.ConeGeometry(largeur, hauteurCone, 60);
+  let cone = new THREE.Mesh(geometry, material);
+
+  cylindre.position.y = hauteurCylindre/2;
+  cone.position.y = hauteurCylindre + hauteurCone/2 ;
+
+  let group = new THREE.Group()
+
+  group.add(cylindre);
+  group.add(cone);
+
+  return group;
+}
+
+var controls = new OrbitControls( camera, renderer.domElement );
+controls.minDistance = 50;
+controls.maxDistance = 10000;
